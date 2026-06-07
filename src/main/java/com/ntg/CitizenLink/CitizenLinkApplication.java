5bfff3d699113e09c894efe9455d8db687abfb1d
Ahmed Khaled <ahmedkhaled910000@gmail.com>
2026-06-07 11:47:27 +0300

    Initial commit: CitizenLink application

---
+++ src/main/java/com/ntg/CitizenLink/CitizenLinkApplication.java
@@ -0,0 +1,13 @@
+package com.ntg.CitizenLink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CitizenLinkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CitizenLinkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ntg/CitizenLink/CitizenLinkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ntg.CitizenLink;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CitizenLinkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
