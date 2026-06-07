4f1d95efe5dea4bb058dd9500feb9c4a57a31121
AhmedX2111 <Ahmedkhaled910000@gmail.com>
2026-06-07 22:16:40 +0300

    remove JwtProperties configuration from application startup

--- src/main/java/com/ntg/CitizenLink/CitizenLinkApplication.java
+++ src/main/java/com/ntg/CitizenLink/CitizenLinkApplication.java
@@ -1,13 +1,11 @@
 package com.ntg.CitizenLink;
 
-import com.ntg.CitizenLink.config.JwtProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
-@EnableConfigurationProperties(JwtProperties.class)
 @EnableCaching
 public class CitizenLinkApplication {
 
