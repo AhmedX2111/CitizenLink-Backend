ab22f6834e39e5a825d96b4f80738ef760164861
abdelrahmanhussien <moody65355@gmail.com>
2026-06-14 11:19:56 +0300

    case created is linked to user with NatID instead of UUID

--- src/main/java/com/ntg/CitizenLink/repositories/CitizenRepository.java
+++ src/main/java/com/ntg/CitizenLink/repositories/CitizenRepository.java
@@ -5,9 +5,12 @@ import com.ntg.CitizenLink.entities.Citizen;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface CitizenRepository extends JpaRepository<Citizen, UUID> {
     boolean existsByNationalId(String nationalId);
+
+    Optional<Citizen> findByNationalId(String nationalId);
 }
