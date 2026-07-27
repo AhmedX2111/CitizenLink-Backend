3db979feb01353ec3e2dfbc0443b8f2457c4452c
Ahmed Khaled <ahmedkhaled910000@gmail.com>
2026-07-27 11:32:31 +0300

    fix some issues

--- src/main/java/com/ntg/CitizenLink/CitizenLinkApplication.java
+++ src/main/java/com/ntg/CitizenLink/CitizenLinkApplication.java
@@ -1,5 +1,6 @@
 package com.ntg.CitizenLink;
 
+import com.ntg.CitizenLink.security.config.CorsProperties;
 import com.ntg.CitizenLink.security.config.JwtProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -9,7 +10,7 @@ import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-@EnableConfigurationProperties(JwtProperties.class)
+@EnableConfigurationProperties({JwtProperties.class, CorsProperties.class})
 public class CitizenLinkApplication {
 
 	public static void main(String[] args) {
